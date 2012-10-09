1538128453c15ac3c6a86af4627902a364a5733e
Rui Da Costa <rui.dacosta@gmail.com>
2012-10-09 11:58:21 -0400

    Adjustments from previously built project

--- src/lilash/sightwords/SightWords.java
+++ src/lilash/sightwords/SightWords.java
@@ -3,18 +3,13 @@ package lilash.sightwords;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import org.apache.cordova.*;
 
-public class SightWords extends Activity {
+public class SightWords extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sight_words);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_sight_words, menu);
-        return true;
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
