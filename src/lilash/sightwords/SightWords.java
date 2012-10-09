c3a3659cd5a850b0da066370d406b8a29048e28f
Rui Da Costa <rui.dacosta@gmail.com>
2012-10-09 12:12:35 -0400

    Still configuring

--- src/lilash/sightwords/SightWords.java
+++ src/lilash/sightwords/SightWords.java
@@ -1,8 +1,6 @@
 package lilash.sightwords;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 import org.apache.cordova.*;
 
 public class SightWords extends DroidGap {
